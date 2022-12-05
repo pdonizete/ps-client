0f7974da7c265b8c941213dc76983629364f04d8
pdonizete <paulo@paulosoft.net>
2022-12-05 07:37:21 -0300

    adicionado um seed inicial do import.sql e criado um endpoint de testes

--- src/main/java/net/paulosoft/client/resources/clientResource.java
+++ src/main/java/net/paulosoft/client/resources/clientResource.java
@@ -1,14 +1,24 @@
 package net.paulosoft.client.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.paulosoft.client.entities.Client;
+import net.paulosoft.client.repositories.ClientRepository;
+
 @RestController
 @RequestMapping("/clients")
 public class clientResource {
+	@Autowired
+	ClientRepository repository;
 @GetMapping
-public String oi() {
-	return "oi";
+public ResponseEntity<List<Client>> oi() {
+	List <Client> lista=repository.findAll();
+	return ResponseEntity.ok().body(lista);
 }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+insert into tb_client(name,cpf,income,birt_date,children) values('Mariaanjo','222.333.444-55','5000','1989-06-15',4);
